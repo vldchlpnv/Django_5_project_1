cf31c1bcfc1fde8b4e0f6bceacdfd6c52e80c43b
Vlad Chulpanov <vlad.chulpanov@icloud.com>
2024-08-28 14:33:30 +0300

    добавил файлы формы и модели + класс модели

--- djvenv/mysite/blog/models.py
+++ djvenv/mysite/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+    body = models.TextField()
+
+    publish = models.DateTimeField(default=timezone.now)  # дата создания
+    created = models.DateTimeField(auto_now_add=True)  # дата публикации
+    updated = models.DateTimeField(auto_now=True) # дата изменения
+
+    class Meta:
+
+        indexes = [models.Index(fields=['-publish'])]
+        ordering = ['-publish'] # сортировка от новых с старым
+
+    def __str__(self):
+        return self.title
+
