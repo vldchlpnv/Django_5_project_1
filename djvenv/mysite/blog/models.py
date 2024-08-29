fbfa8e6c74afa0833f1afcc6d54cf808056bd9e6
Vlad Chulpanov <vlad.chulpanov@icloud.com>
2024-08-29 19:05:05 +0300

    добавил один ко многим

--- djvenv/mysite/blog/models.py
+++ djvenv/mysite/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -9,7 +9,9 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
+
 
     publish = models.DateTimeField(default=timezone.now)  # дата создания
     created = models.DateTimeField(auto_now_add=True)  # дата публикации
