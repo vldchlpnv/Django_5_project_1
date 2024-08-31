d125eb19ff61f540c2226f5094b1198f9da2edb4
Vlad Chulpanov <vlad.chulpanov@icloud.com>
2024-08-31 19:51:20 +0300

    кое что добавил

--- djvenv/mysite/blog/views.py
+++ djvenv/mysite/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def post_list(request):    # подтянем модель
+
+    post = Post.publish.all()
+    return render(request, 'blog/post/list.html', {'post':post})
+
+def post_detail(request, id):
+
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post':post})
+
 
 # Create your views here.
