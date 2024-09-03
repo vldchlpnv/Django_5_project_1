909dff6d69d40417cb9081160fcf4e8416c24996
Vlad Chulpanov <vlad.chulpanov@icloud.com>
2024-09-03 23:31:16 +0300

    добавил  канонические URL

--- djvenv/mysite/blog/models.py
+++ djvenv/mysite/blog/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queruset(self):
         return super().get_queryset().filter(status=Post.status.PABLISHED)
 class Post(models.Model):
+
+
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
 
@@ -29,4 +32,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
+
 
