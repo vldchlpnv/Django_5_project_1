5c84b918f113f3807d180562704ee0bd77103ba2
Vlad Chulpanov <vlad.chulpanov@icloud.com>
2024-09-05 17:29:39 +0300

    добавил класс постраничной разбивки

--- djvenv/mysite/blog/views.py
+++ djvenv/mysite/blog/views.py
@@ -1,10 +1,15 @@
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 def post_list(request):    # подтянем модель
 
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts':posts})
 
+   
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
                              status=Post.Status.PUBLISHED,
